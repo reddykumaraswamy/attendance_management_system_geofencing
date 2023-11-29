5e2edcc85a993e15cd84f0d416967dba93632784
Reddy Kumaraswamy <65892552+reddykumaraswamy@users.noreply.github.com>
2023-11-29 23:54:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def speak(str1):
     speak.Speak(str1)
 
 video = cv2.VideoCapture(0)
-facedetect = cv2.CascadeClassifier('C:/Users/reddy/PycharmProjects/Face_Recognition_Based_Attendance_System/data/haarcascades/haarcascade_frontalface_default.xml')
+facedetect = cv2.CascadeClassifier('./data/haarcascades/haarcascade_frontalface_default.xml')
 
 with open('data/names.pkl', 'rb') as f:
     LABELS = pkl.load(f)
